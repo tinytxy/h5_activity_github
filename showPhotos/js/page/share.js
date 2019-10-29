2add287543f204b152f1ca98deb337669a2f32e9
tanxinyu <tanxinyu@icaremgt.com>
2019-10-29 16:26:02 +0800

    微信分享开启debugger模式

--- showPhotos/js/page/share.js
+++ showPhotos/js/page/share.js
@@ -3,7 +3,7 @@ var _settings = rules.settings;
 var sdk = typeof jsSdk === "undefined" ? "" : jsSdk;
 
 wx.config({
-    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: (sdk ? sdk.appId : ''), // 必填，公众号的唯一标识
     timestamp: (sdk ? sdk.timestamp : ''), // 必填，生成签名的时间戳
     nonceStr:  (sdk ? sdk.noncestr : ''), // 必填，生成签名的随机串
@@ -71,3 +71,8 @@ wx.ready(function(){
         }
     });
 });
+
+wx.error(function(res) {
+    console.log('错误信息 === ')
+    alert(JSON.stringify(res))
+})
