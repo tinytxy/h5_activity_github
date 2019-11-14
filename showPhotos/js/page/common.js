2bb2d33ac94312db86be630000d408dbbc53bbd8
tanxinyu <tanxinyu@icaremgt.com>
2019-11-14 14:40:54 +0800

    定位错误

--- showPhotos/js/page/common.js
+++ showPhotos/js/page/common.js
@@ -704,56 +704,59 @@ function setWxShare(data) {
 
 // 图片压缩
 function compressImage(file, callback) {
+  try {
+    var reader = new FileReader();
+    var img = new Image();
 
-  var reader = new FileReader();
-  var img = new Image();
+    reader.readAsDataURL(file);
+    
+    // 缩放图片需要的canvas
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
 
-  reader.readAsDataURL(file);
-  
-  // 缩放图片需要的canvas
-  var canvas = document.createElement('canvas');
-  var context = canvas.getContext('2d');
+    img.onload = function() {
+      // 图片原始尺寸
+      var originWidth = this.width;
+      var originHeight = this.height;
+      // 最大尺寸限制
+      var maxWidth = 800, 
+          maxHeight = 800;
 
-  img.onload = function() {
-    // 图片原始尺寸
-    var originWidth = this.width;
-    var originHeight = this.height;
-    // 最大尺寸限制
-    var maxWidth = 800, 
-        maxHeight = 800;
+      // 目标尺寸
+      var targetWidth = originWidth, 
+          targetHeight = originHeight;
 
-    // 目标尺寸
-    var targetWidth = originWidth, 
-        targetHeight = originHeight;
-
-    // 图片尺寸超过1024x1024的限制
-    if (originWidth > maxWidth || originHeight > maxHeight) {
-      if (originWidth / originHeight > maxWidth / maxHeight) {
-          // 更宽，按照宽度限定尺寸
-          targetWidth = maxWidth;
-          targetHeight = Math.round(maxWidth * (originHeight / originWidth));
-      } else {
-          targetHeight = maxHeight;
-          targetWidth = Math.round(maxHeight * (originWidth / originHeight));
+      // 图片尺寸超过1024x1024的限制
+      if (originWidth > maxWidth || originHeight > maxHeight) {
+        if (originWidth / originHeight > maxWidth / maxHeight) {
+            // 更宽，按照宽度限定尺寸
+            targetWidth = maxWidth;
+            targetHeight = Math.round(maxWidth * (originHeight / originWidth));
+        } else {
+            targetHeight = maxHeight;
+            targetWidth = Math.round(maxHeight * (originWidth / originHeight));
+        }
       }
-    }
-        
-    // canvas对图片进行缩放
-    canvas.width = targetWidth;
-    canvas.height = targetHeight;
-    // 清除画布
-    context.clearRect(0, 0, targetWidth, targetHeight);
-    // 图片压缩
-    context.drawImage(img, 0, 0, targetWidth, targetHeight);
-    // canvas转为blob并上传
-    canvas.toBlob(function (blob) {
-      callback && callback(blob);     
-    }, file.type || 'image/png');
-  };
+          
+      // canvas对图片进行缩放
+      canvas.width = targetWidth;
+      canvas.height = targetHeight;
+      // 清除画布
+      context.clearRect(0, 0, targetWidth, targetHeight);
+      // 图片压缩
+      context.drawImage(img, 0, 0, targetWidth, targetHeight);
+      // canvas转为blob并上传
+      canvas.toBlob(function (blob) {
+        callback && callback(blob);     
+      }, file.type || 'image/png');
+    };
 
-  reader.onload = function(e) {
-    img.src = e.target.result;
-  };
+    reader.onload = function(e) {
+      img.src = e.target.result;
+    };
+  } catch (error) {
+    alert(error)
+  }
 }
 
 /* ---------------------补充功能逻辑--------------------- */
